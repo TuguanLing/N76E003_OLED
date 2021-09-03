ef46ab465896582e850603970f4822cab031e681
TuguanLing <57609069+TuguanLing@users.noreply.github.com>
2021-09-03 18:20:18 +0800

    更新delay.c

--- Common/Delay.c
+++ Common/Delay.c
@@ -1,18 +1,4 @@
-/*---------------------------------------------------------------------------------------------------------*/
-/*                                                                                                         */
-/* Copyright(c) 2016 Nuvoton Technology Corp. All rights reserved.                                         */
-/*                                                                                                         */
-/*---------------------------------------------------------------------------------------------------------*/
-
-//***********************************************************************************************************
-//  Nuvoton Technoledge Corp. 
-//  Website: http://www.nuvoton.com
-//  E-Mail : MicroC-8bit@nuvoton.com
-//  Date   : Apr/21/2016
-//***********************************************************************************************************
-
 #include "N76E003.h"
-
 
 bit BIT_TMP;
 
@@ -106,6 +92,22 @@ void Timer3_Delay10us(UINT32 u32CNT)
     {
         RL3 = LOBYTE(TIMER_DIV4_VALUE_10us); //Find  define in "Function_define.h" "TIMER VALUE"
         RH3 = HIBYTE(TIMER_DIV4_VALUE_10us);
+        while ((T3CON&SET_BIT4) != SET_BIT4);		//Check Timer3 Time-Out Flag
+        clr_TF3;
+        u32CNT --;
+    }
+    clr_TR3;                                		//Stop Timer3
+}
+//------------------------------------------------------------------------------
+void Timer3_Delay1s(UINT32 u32CNT)
+{
+    T3CON = 0x07;                           		//Timer3 Clock = Fsys/128
+    set_TR3;                                		//Trigger Timer3
+	u32CNT = u32CNT*10;
+    while (u32CNT != 0)
+    {
+        RL3 = LOBYTE(TIMER_DIV128_VALUE_100ms); //Find  define in "Function_define.h" "TIMER VALUE"
+        RH3 = HIBYTE(TIMER_DIV128_VALUE_100ms);
         while ((T3CON&SET_BIT4) != SET_BIT4);		//Check Timer3 Time-Out Flag
         clr_TF3;
         u32CNT --;
--- Include/Delay.h
+++ Include/Delay.h
@@ -1,3 +1,6 @@
+#ifndef __DELAY_H
+#define __DELAY_H
+
 void Timer0_Delay100us(UINT32 u32CNT);
 void Timer0_Delay1ms(UINT32 u32CNT);
 void Timer1_Delay10ms(UINT32 u32CNT);
@@ -6,3 +9,7 @@ void Timer3_Delay100ms(UINT32 u32CNT);
 
 void Timer0_Delay40ms(UINT32 u32CNT);
 void Timer3_Delay10us(UINT32 u32CNT);
+
+void Timer3_Delay1s(UINT32 u32CNT);
+
+#endif
